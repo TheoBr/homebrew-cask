9e48dfc654b8924592a6d62f75e1f91f8fb3aa6d
Theo Browne <me@theo.li>
2016-01-06 13:11:37 -0500

    BetterTouchTool is no longer versioned on their official download

--- Casks/bettertouchtool.rb
+++ Casks/bettertouchtool.rb
@@ -6,12 +6,12 @@ cask 'bettertouchtool' do
     # bettertouchtool.com is the official download host per the vendor homepage
     url "http://bettertouchtool.net/btt#{version}.zip"
   else
-    version '1.5'
-    sha256 'a0ea79136322c634545c0b9da6001944fad5ec734b891fbc63c5485ba0cfd516'
+    version :latest
+    sha256 :no_check
 
     url "https://bettertouchtool.com/releases/BetterTouchTool.zip"
     appcast 'http://appcast.boastr.net',
-            :sha256 => '1cb51646c9d3a28ec6f6904f04d83a3d73aface7ca055793439a2746d8f2a112'
+            :sha256 => 'f6408c0ba48588cd547bef4abdcfd723af5133af141f9f7a146d9ee7721f86e6'
   end
 
   name 'BetterTouchTool'
