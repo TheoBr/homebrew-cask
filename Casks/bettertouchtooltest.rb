d481ecf64bc4c1037a4fff5ca8cc220046b128ea
Theo Browne <me@theo.li>
2016-01-06 13:18:16 -0500

    Testing

---
+++ Casks/bettertouchtooltest.rb
@@ -0,0 +1,32 @@
+cask 'bettertouchtooltest' do
+  if MacOS.release <= :snow_leopard
+    version '0.939'
+    sha256 'fad5e9d36259c379bdb33188cf15d179fd9ff73023035c98f5734e7e3e13bb75'
+
+    # bettertouchtool.com is the official download host per the vendor homepage
+    url "http://bettertouchtool.net/btt#{version}.zip"
+  else
+    version :latest
+    sha256 :no_check
+
+    url "https://bettertouchtool.com/releases/BetterTouchTool.zip"
+    appcast 'http://appcast.boastr.net',
+            :sha256 => 'f6408c0ba48588cd547bef4abdcfd723af5133af141f9f7a146d9ee7721f86e6'
+  end
+
+  name 'BetterTouchTool'
+  homepage 'http://boastr.net/'
+  license :gratis
+
+  auto_updates true
+  accessibility_access true
+
+  app 'BetterTouchTool.app'
+
+  uninstall :login_item => 'BetterTouchTool'
+
+  zap :delete => [
+                   '~/Library/Preferences/com.hegenberg.BetterTouchTool.plist',
+                   '~/Library/Application Support/BetterTouchTool',
+                 ]
+end
